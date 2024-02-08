9331f627ac4fb1effa07bf668bf0633fe201ff62
sudhanshu2504 <104723065+sudhanshu2504@users.noreply.github.com>
2024-02-08 21:40:42 +0530

    Add files via upload

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-// const mongoURI = "mongodb://localhost:27017/gamezone";
-// const mongoURI = "mongodb+srv://sudhanshu250403:mongoDBatlas@cluster0.zajcgga.mongodb.net/gamezone";
+require('dotenv').config();
 
 const functionn = ()=>{
     console.log("Connect to mongoDB Successfully");
 }
 
 const connnectToMongoDB = ()=>{
-    mongoose.connect(process.env.MONGO_URI,functionn())
+    mongoose.connect(process.env.MONGO_URL,functionn())
 }
 module.exports = connnectToMongoDB;
