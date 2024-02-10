9db9652331e424383b2ac90e31928857c2d4824a
sudhanshu2504 <104723065+sudhanshu2504@users.noreply.github.com>
2024-02-10 10:48:24 +0530

    Update User.js
    city added

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
     email : {type: String, required:true, unique:true},
     password : {type: String, required:true},
     address : {type: String, default:""},
+    city : {type: String, default:""},
     pincode : {type: String, default:""},
     contact : {type: String, default:""},
 }, {timestamps : true} )
